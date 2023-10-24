8d548033d31e81611911c6171d06b59cc4445544
fabioPuissant <fabio.puissant@hotmail.com>
2023-10-24 21:57:41 +0200

    Home page | product grid added

--- store/src/app/pages/home/home.component.ts
+++ store/src/app/pages/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+
+const ROWS_HEIGHT: {[id:number]: number} = {1: 400, 3: 355, 4: 350} // says it is an object with key 'id' that is a number, the value to which the key maps is a number
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  category: string|undefined;
+  rowHeight = ROWS_HEIGHT[this.cols]; // this is a nice to know
 
   constructor() {}
   
@@ -14,7 +19,13 @@ export class HomeComponent implements OnInit {
   }
   
   onColumnsCountChange(colsNumber: number) {
-    if (colsNumber == 1 || colsNumber == 3 || colsNumber == 4  )
+    if (colsNumber == 1 || colsNumber == 3 || colsNumber == 4  ){
       this.cols = colsNumber
+
+      this.rowHeight = ROWS_HEIGHT[this.cols];
+    }
+  }
+  onShowCategory(chosenCategory: string): void {
+    this.category = chosenCategory;
   }
 }
